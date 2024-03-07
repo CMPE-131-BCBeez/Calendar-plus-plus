449494c11a070e483b2c5c429ec7d59eac3f7817
rudysuarez1 <rudysuarez.ita@gmail.com>
2024-03-06 23:02:25 -0800

    this will do nothing, just added the user_email atribute to the sql table

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS Users (
     id INTEGER PRIMARY KEY,
     user_name TEXT NOT NULL,
+    user_email TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
@@ -20,3 +21,5 @@ CREATE TABLE IF NOT EXISTS UsersEvents (
     FOREIGN KEY (event_id) REFERENCES Events(id),
     PRIMARY KEY (user_id, event_id)
 );
+
+CREATE TABLE IF NOT EXISTS
