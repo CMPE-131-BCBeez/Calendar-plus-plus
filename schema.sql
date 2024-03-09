462bb4163d4511eaee24f51877d29e4c96b7c3a2
rudysuarez1 <rudysuarez.ita@gmail.com>
2024-03-09 11:26:02 -0800

    I updated the Users table a bit and added the Calendars table and added a few more things to the Events table

--- schema.sql
+++ schema.sql
@@ -1,16 +1,28 @@
 -- SQLITE3
 
 CREATE TABLE IF NOT EXISTS Users (
-    id INTEGER PRIMARY KEY,
-    user_name TEXT NOT NULL,
-    user_email TEXT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL
+    user_id INTEGER AUTOINCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) UNIQUE NOT NULL,
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Calendars(
+    calendar_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    calendar_name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    );
+
 CREATE TABLE IF NOT EXISTS Events (
-    id INTEGER PRIMARY KEY,
-    event_name TEXT NOT NULL
+    event_id INTEGER PRIMARY KEY,
+    calendar_id INT,
+    event_name VARCHAR(50) NOT NULL,
+    event_details TEXT,
+    event_start DATETIME NOT NULL,
+    event_end DATETIME NOT NULL,
+    FOREIGN KEY (calendar_id) REFERENCES Calendars(calendar_id);
     event_type TEXT CHECK (event_type IN ('EVENT', 'REMINDER')) NOT NULL
 );
 
@@ -22,4 +34,4 @@ CREATE TABLE IF NOT EXISTS UsersEvents (
     PRIMARY KEY (user_id, event_id)
 );
 
-CREATE TABLE IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS 
