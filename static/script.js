f7aa44c7158376f73f47f60b0453629825584942
Mamatyari700 <pandapool.1991.3@gmail.com>
2024-04-15 02:14:03 -0700

    installed Fullcalender(framework? plugin?)

--- static/script.js
+++ static/script.js
@@ -1,17 +1,8 @@
-function generateCalendarGrid(data) {
-    const calendarGrid = document.getElementById('calendarGrid');
-
-    calendarGrid.innerHTML = '';
-  
-    data.forEach(week => {
-      const weekRow = document.createElement('div');
-      weekRow.classList.add('weekRow');
-      week.forEach(day => {
-        const dayCell = document.createElement('div');
-        dayCell.classList.add('dayCell');
-        dayCell.textContent = day;
-        weekRow.appendChild(dayCell);
-      });
-      calendarGrid.appendChild(weekRow);
-    });
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    // カレンダーの設定
+    plugins: [ 'dayGrid' ] 
+  });
+  calendar.render(); 
+});
