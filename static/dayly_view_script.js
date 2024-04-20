75a9176d93dcde97b67626a4950bf06992723433
Mamatyari700 <pandapool.1991.3@gmail.com>
2024-04-19 19:22:48 -0700

    Squashed commit of the following:
    commit 9cb3c9624857c62d89bdba771892b3f4c06f71a9
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Fri Apr 19 19:10:25 2024 -0700
    
        ready for merge
    
    commit 1e937c4bf58321acbd2320b4cdbc52bebcc5af32
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Thu Apr 18 21:35:15 2024 -0700
    
        working on style
    
    commit 7c002e3eeca32ab4a22020b39454ba0e46764ce2
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Thu Apr 18 10:07:23 2024 -0700
    
        yay
    
    commit 8af3ee4a985914519ebf845e95f2cc612721ffa1
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Wed Apr 17 20:50:27 2024 -0700
    
        started working on the dailyview
    
    commit ba05cbc3e8b6ea4c286264ecf52bffa78643a031
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Wed Apr 17 12:50:26 2024 -0700
    
        start work on dayly view
    
    commit 39fbb64947050aace4f349d00d5b3b693f8004f6
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Wed Apr 17 11:15:44 2024 -0700
    
        added dayly view files, fixed styling to fit the screen
    
    commit afc3ff539d8eab09cefb7705af9a99803e9eff64
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Tue Apr 16 21:10:02 2024 -0700
    
        added setting button
    
    commit e4cf6f4e43af1f281ac0f9225d6751dff84f68e4
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Tue Apr 16 20:35:37 2024 -0700
    
        added images
    
    commit dd5c96f8e7511be7a43137f3153fc9a92d8db9f3
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Tue Apr 16 19:24:37 2024 -0700
    
        tested with flask and checked it works!!!
    
    commit 64fb3d893f2c132d1a7e606e12c250504297707f
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Tue Apr 16 16:07:10 2024 -0700
    
        more styling and delete css's desctiption on each line because it's annoying
    
    commit 86a398db79e9105eb983a24d3095a3f5ca75b329
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Tue Apr 16 12:29:50 2024 -0700
    
        edit more css
    
    commit d626813722eacea5aa3685414a0e230c4f9bdd68
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Mon Apr 15 21:05:06 2024 -0700
    
        fixed logical issue
    
    commit 8f4c90695d2d434bdd3d39560687613535ef6ec7
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Mon Apr 15 20:15:36 2024 -0700
    
        added javascript and css
    
    commit f7aa44c7158376f73f47f60b0453629825584942
    Author: Mamatyari700 <pandapool.1991.3@gmail.com>
    Date:   Mon Apr 15 02:14:03 2024 -0700
    
        installed Fullcalender(framework? plugin?)

---
+++ static/dayly_view_script.js
@@ -0,0 +1,6 @@
+let today = new Date();
+let current_day = today.getDate();
+let current_month = today.getMonth() + 1;
+let current_year = today.getFullYear();
+
+let daily_
---
+++ static/monthly_view_script.js
@@ -0,0 +1,117 @@
+let today = new Date();
+let current_day = today.getDate();
+let current_month = today.getMonth() + 1;
+let current_year = today.getFullYear();
+
+function generate_calendar(year, month) {
+  let first_date = new Date(year, month - 1, 1);
+  let last_day = new Date(year, month, 0);
+  let prev_month_last_day = new Date(year, month - 1, 0);
+  let next_month_mergin = 6 - last_day.getDay();
+
+  //Header of the calendar
+let monthly_calendar = '<table>';
+  monthly_calendar += '<thead>';
+  monthly_calendar += '<tr class="calendar_day">';
+  monthly_calendar += '<th class="calendar_day">Sun</th>';
+  monthly_calendar += '<th class="calendar_day">Mon</th>';
+  monthly_calendar += '<th class="calendar_day">Tue</th>';
+  monthly_calendar += '<th class="calendar_day">Wed</th>';
+  monthly_calendar += '<th class="calendar_day">Thu</th>';
+  monthly_calendar += '<th class="calendar_day">Fri</th>';
+  monthly_calendar += '<th class="calendar_day">Sat</th>';
+  monthly_calendar += '</tr>';
+  monthly_calendar += '</thead>';
+
+  //body of the calendar
+  monthly_calendar += '<tbody>';
+  let day_of_Week = first_date.getDay();
+  let prev_next_day_class = 'calendar_basic';
+
+  //day block start
+  monthly_calendar += '<tr>';
+
+  //fill the blank before the 1st date 
+  for (let i = 0; i < day_of_Week; i++) {
+    monthly_calendar += '<td class="'+ prev_next_day_class +'">' + (prev_month_last_day.getDate() - day_of_Week + 1 + i) + '</td>';
+  }
+
+  //fill the date and change the color of today's cell
+  for (let day = 1; day <= last_day.getDate(); day++) {
+    let cell_class = 'calendar_basic';
+    if (year === current_year && month === current_month && day === current_day) {
+      cell_class += 'today';
+    }
+   
+    monthly_calendar += '<td class="' + cell_class + '">' + day + '</td>';
+    if (first_date.getDay() === 6) {
+      monthly_calendar += '</tr><tr>';
+    }
+    first_date.setDate(first_date.getDate() + 1);
+  }
+
+  //fill the blank after the last day
+  if (first_date.getDay() !== 0 || next_month_mergin > 0) {
+    for (let i = 0; i < next_month_mergin; i++) {
+      monthly_calendar += '<td class="'+ prev_next_day_class +'">' + (i + 1) + '</td>';
+    }
+  }
+
+  monthly_calendar += '</tr>';
+  monthly_calendar += '</tbody>';
+  monthly_calendar += '</table>';
+
+  document.getElementById('monthly_calendar').innerHTML = monthly_calendar;
+  let todayCell = document.querySelector('.calendar_basic.today');
+    if (todayCell) {
+      let todayCellNumber = todayCell.innerText;
+        todayCell.innerHTML = '<span>' + todayCellNumber + '</span>';
+}
+}
+
+//implement the button going back to previous month
+document.getElementById('prev_month').addEventListener('click', function() {
+  current_month--;
+  if (current_month === 0) {
+    current_year--;
+    current_month = 12;
+  }
+  generate_calendar(current_year, current_month);
+  year_month(current_month, current_year);
+});
+
+//implement the button going forward to next month
+document.getElementById('next_month').addEventListener('click', function() {
+  current_month++;
+  if (current_month === 13) {
+    current_year++;
+    current_month = 1;
+  }
+  generate_calendar(current_year, current_month);
+  year_month(current_month, current_year);
+});
+
+function year_month(month, year){
+  let englishMonth = new Date(year, month - 1, 1).toLocaleString('en', { month: 'long' });
+  let year_month_header = englishMonth + '/' + year;
+  document.getElementById('year_month_header').innerHTML = year_month_header;
+};
+
+document.getElementById('today_button').addEventListener('click', function() {
+  current_month = today.getMonth() + 1;
+  current_year = today.getFullYear();
+  generate_calendar(current_year, current_month);
+  year_month(current_month, current_year);
+});
+
+year_month(current_month, current_year);
+generate_calendar(current_year, current_month);
+document.querySelectorAll('.calendar_basic').forEach(cell => {
+  cell.addEventListener('click', function() {
+    let day = this.innerText;
+    let url = '/daily_calendar?date=' + current_year + '-' + current_month + '-' + day;
+    window.location.href = url;
+  });
+});
+
+day(current_day);
