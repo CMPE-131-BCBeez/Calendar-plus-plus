b6135e32c7cfe2f23cc1a4c999508be3329d493a
Mamatyari700 <pandapool.1991.3@gmail.com>
2024-04-23 00:06:41 -0700

    started working on the daily view

--- static/dayly_view_script.js
+++ static/dayly_view_script.js
@@ -3,6 +3,26 @@ let current_day = today.getDate();
 let current_month = today.getMonth() + 1;
 let current_year = today.getFullYear();
 
+function time_to_timestamp(current_day) {
+    var date = new Date(timestamp * 1000);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    // 時間と分を文字列として組み立てる
+    var timeString = hours + ':' + (minutes < 10 ? '0' : '') + minutes; // 分が10未満の場合は0を追加
+    return timeString;
+}
+
+// サンプルのUnixタイムスタンプ
+var unixTimestamp = 1618911600; // 2021-04-20 12:00:00 PM UTC
+
+// Unixタイムスタンプを時間の文字列に変換して表示
+console.log(timestampToTime(unixTimestamp))
+let unix_time = 
+
+function generate_daily_view(){
+    let beggining_day = 
+}
+
 //redirect to daily view when user click on the calender cell
 function redirect_to_daily_view(date) {
     //add date in url
